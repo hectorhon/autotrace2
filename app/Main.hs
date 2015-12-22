4bf83219bf896d382f41859f64f4f38ac8ee032d
Hector Hon <hon.lianhung@gmail.com>
2015-12-22 22:59:26 +0800

    Servant

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
+import Servant
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Data.Text
+
+type TestAPI = "test" :> Get '[PlainText] Text
+
 main :: IO ()
-main = undefined
+main = run 3000 app
+
+app :: Application
+app = serve testAPI server
+
+server :: Server TestAPI
+server = return "test"
+
+testAPI :: Proxy TestAPI
+testAPI = Proxy
