09165734060a21546ea5f39d7501d20b19a7accc
hectorhon <hon.lianhung@gmail.com>
2015-12-29 08:51:40 +0800

    Fix counts dropping last result in array

--- src/TimeSeriesData/Counts.hs
+++ src/TimeSeriesData/Counts.hs
@@ -14,7 +14,8 @@ counts ss = if null times then [] else counts' 0 (fst $ head times) times
 
 counts' :: Int -> NominalDiffTime -> [(NominalDiffTime, StartEnd)]
         -> [(TSInterval, Int)]
-counts' acc _ ((_, pos):[]) = assert (acc == 1 && pos == End) []
+counts' acc time ((now, pos):[]) = assert (acc == 1 && pos == End) $
+  if now == time then [] else [((time, now), acc)]
 counts' acc time ((now, pos):times)
   | now == time = counts' (if pos == Start then acc + 1 else acc - 1) now times
   | now > time = ((time, now), acc)
