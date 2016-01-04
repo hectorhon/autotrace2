85149a391499617f974a64d398e25957c8f5aa1f
hectorhon <hon.lianhung@gmail.com>
2016-01-04 15:40:23 +0800

    Fix indentation in block view

--- app/Block/Views.hs
+++ app/Block/Views.hs
@@ -14,10 +14,10 @@ uploadBlockConfigPage :: UTCTime -> Html
 uploadBlockConfigPage uploadDate = layout "Upload block config file" $ do
   h1 "Upload block config file"
   H.form ! method "post" ! enctype "multipart/form-data" $ do
-  H.label $ do
-    H.span "Snapshot date"
-    datepicker "date-field" "snapshotdate" (formatDay uploadDate)
-  field "Group" "group" (\ _ -> "" :: String) Nothing
-  input ! Ha.name "blockconfig" ! type_ "file"
-  button "Save"
-  cancelButton "upload-block-config-cancel-button"
+    H.label $ do
+      H.span "Snapshot date"
+      datepicker "date-field" "snapshotdate" (formatDay uploadDate)
+    field "Group" "group" (\ _ -> "" :: String) Nothing
+    input ! Ha.name "blockconfig" ! type_ "file"
+    button "Save"
+    cancelButton "upload-block-config-cancel-button"
